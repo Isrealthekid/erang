ff3acd757f3936d8b7dbae074921b80badaea860
isrealthekid <oluwoleisreal12@gmail.com>
2024-10-20 22:50:15 +0100

    testimonials

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,76 @@
 import React from 'react'
+import './Testimonials.css'
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    
+    <section class="testimonials">
+      <div className='testimonials-title'>
+      <h2>Testimonials</h2>
+      <h3>What People are Saying</h3>
+      </div>
+   
+    <ul class="testimonials-list has-scrollbar">
+        <li class="testimonials-item">
+            <div class="content-card" data-testimonials-item>
+                {/* <figure class="testimonials-avatar-box">
+                    <img src="" alt="User 1" width="60" data-testimonials-avatar />
+                </figure> */}
+                <h4 class="h4 testimonials-item-title" data-testimonials-title>
+                    Daniel Mac
+                </h4>
+                <div class="testimonials-text data-testimonials-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti impedit facere sint quam delectus autem eaque laboriosam amet. Accusamus, enim aliquam. Aliquid numquam debitis impedit, nam dolorem tenetur! Quae repellat omnis obcaecati delectus, adipisci autem tenetur perferendis dolores inventore, atque laudantium suscipit eos incidunt, quisquam quo veniam! Adipisci, expedita quos.</p>
+                </div>
+            </div>
+        </li>
+
+        <li class="testimonials-item">
+            <div class="content-card" data-testimonials-item>
+                {/* <figure class="testimonials-avatar-box">
+                    <img src="" alt="User 1" width="60" data-testimonials-avatar />
+                </figure> */}
+                <h4 class="h4 testimonials-item-title" data-testimonials-title>
+                    Daniel Mac
+                </h4>
+                <div class="testimonials-text" data-testimonials-text>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti impedit facere sint quam delectus autem eaque laboriosam amet. Accusamus, enim aliquam. Aliquid numquam debitis impedit, nam dolorem tenetur! Quae repellat omnis obcaecati delectus, adipisci autem tenetur perferendis dolores inventore, atque laudantium suscipit eos incidunt, quisquam quo veniam! Adipisci, expedita quos.</p>
+                        
+                </div>
+            </div>
+        </li>
+
+        <li class="testimonials-item">
+            <div class="content-card" data-testimonials-item>
+                {/* <figure class="testimonials-avatar-box">
+                    <img src="" alt="User 1" width="60" data-testimonials-avatar/>
+                </figure> */}
+                <h4 class="h4 testimonials-item-title" data-testimonials-title>
+                    Daniel Mac
+                </h4>
+                <div class="testimonials-text "data-testimonials-text>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti impedit facere sint quam delectus autem eaque laboriosam amet. Accusamus, enim aliquam. Aliquid numquam debitis impedit, nam dolorem tenetur! Quae repellat omnis obcaecati delectus, adipisci autem tenetur perferendis dolores inventore, atque laudantium suscipit eos incidunt, quisquam quo veniam! Adipisci, expedita quos.</p>
+                        
+                </div>
+            </div>
+        </li>
+
+        <li class="testimonials-item">
+            <div class="content-card" data-testimonials-item>
+                {/* <figure class="testimonials-avatar-box">
+                    <img src="" alt="User 1" width="60" data-testimonials-avatar />
+                </figure> */}
+                <h4 class="h4 testimonials-item-title" data-testimonials-title>
+                    Daniel Mac
+                </h4>
+                <div class="testimonials-text" data-testimonials-text>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti impedit facere sint quam delectus autem eaque laboriosam amet. Accusamus, enim aliquam. Aliquid numquam debitis impedit, nam dolorem tenetur! Quae repellat omnis obcaecati delectus, adipisci autem tenetur perferendis dolores inventore, atque laudantium suscipit eos incidunt, quisquam quo veniam! Adipisci, expedita quos.</p>                        
+                </div>
+            </div>
+        </li>
+    </ul>
+
+ </section>
   )
 }
 
